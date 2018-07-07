dc0153c8257c330d7ccd6268ccba31371313f389
belsyuen <email-me@belsy.space>
2018-07-07 13:54:59 +0800

    initial FE

---
+++ public/js/app.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+    checkLogin().then(displayName=>{
+      getUserinfo(displayName)
+    })
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 DATABASE 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 AUTHENTICATION 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+
+function checkLogin(){
+  return new Promise((res, rej) =>{
+
+  const user = firebase.auth().onAuthStateChanged(user=>{
+    console.log("checking user: ", JSON.stringify(user))
+    if (!user){
+      googleLogin()
+    }
+    else{
+      console.log("Username: ", user.displayName)
+      res(user.displayName)
+    }
+  });
+})
+}
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+          const user = result.user;
+          console.log("user: ", user)
+      })
+      .catch(console.log)
+}
+
+
+function getUserinfo(displayName){
+  const db = firebase.firestore();
+  db.collection.get().then(querySnapshot=>{
+    querySnapshot.forEach(query =>{
+      console.log("query: ", query)
+    })
+  })
+}
