1d2d0b9e44a23b550504dbb1612811b78d861872
neo.alienson <neo@orz.hk>
2018-07-08 10:55:24 +0800

    complete order process

---
+++ firebase/public/js/order-processing.js
@@ -0,0 +1,80 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+  // // The Firebase SDK is initialized and available here!
+  //
+  // firebase.auth().onAuthStateChanged(user => { });
+  // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
+  // firebase.messaging().requestPermission().then(() => { });
+  // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
+  //
+  // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+    // checkLogin().then(displayName=>{
+    //   getUserinfo(displayName)
+    // })
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 DATABASE 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+  const db = firebase.firestore();
+  const settings = {/* your settings... */ timestampsInSnapshots: true};
+  db.settings(settings);
+  // will retrieve from query once we have multiple mat printed after demo
+  id = window.location.href.split('=')[1].toString();
+  db.collection("orders").doc(id)
+      .onSnapshot(function(doc) {
+          var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+          console.log(source, " data: ", doc.data());
+          if (doc.data() == null) {
+              window.location.replace('order-done.html?id=' + id);
+          }
+      });
+
+
+
+
+
+// // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 AUTHENTICATION 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+
+function checkLogin(){
+  return new Promise((res, rej) =>{
+
+  const user = firebase.auth().onAuthStateChanged(user=>{
+    console.log("checking user: ", JSON.stringify(user))
+    if (!user){
+      googleLogin()
+    }
+    else{
+      console.log("Username: ", user.displayName)
+      res(user.displayName)
+    }
+  });
+})
+}
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+          const user = result.user;
+          console.log("user: ", user)
+      })
+      .catch(console.log)
+}
+
+
+function getUserinfo(displayName){
+  const db = firebase.firestore();
+  db.collection.get().then(querySnapshot=>{
+    querySnapshot.forEach(query =>{
+      console.log("query: ", query)
+    })
+  })
+}
