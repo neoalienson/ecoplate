c02f8bd56e3f64022f09b384c5c46ea2cf798052
neo.alienson <neo@orz.hk>
2018-07-08 10:31:42 +0800

    add leftover calculation. fix bug.

--- pi/adc.py
+++ pi/adc.py
@@ -11,79 +11,81 @@ cred = credentials.Certificate("/home/pi/AngelHack/SunFounder_SensorKit_for_RPi2
 firebase_admin.initialize_app(cred)
 
 db = firestore.Client()
-order_ref = db.collection(u'orders')
+order_ref = db.collection(u'done_orders')
 weightError = 1
 
 def setup():
-        ADC.setup(0x48)
+	ADC.setup(0x48)
 
 def scanIfOrderAssociated():
-        try:
-                orderItems = order_ref.get()
-                for docs in orderItems:
-                        #print(u'Document data: {}'.format(docs.to_dict()))
-                        try:
-                                customerName = docs.get(u'consumer').get().get(u'name')
-                                initWeight = ADC.read(1)
-                                dishNotServed = True
-                                while dishNotServed:
-                                        currentWeight = ADC.read(1)
-                                        if currentWeight > (initWeight + 4):
-                                                dishNotServed = False
-                                        else:
-                                                print(u'Waiting for the dish, not sending data')
-                                                print ADC.read(1)
-                                        time.sleep(0.3)
-                                print(u'dish is served')
+	try:
+		orderItems = order_ref.get()
+		for docs in orderItems:
+			print(u'Document data: {}'.format(docs.to_dict()))
+			try:
+				initWeight = ADC.read(1)
+				dishNotServed = True
+				while dishNotServed:
+					currentWeight = ADC.read(1)
+					if currentWeight > (initWeight + 4):
+						dishNotServed = False
+					else:
+						print(u'Waiting for the dish, not sending data')
+						print ADC.read(1)
+					time.sleep(0.3)
+				print(u'dish is served')
 
-                                weightArray = []
-                                previousWeight = ADC.read(1)
-                                count = 0
-                                foodNotFinished = True
-                                while foodNotFinished:
-                                        currentWeight = ADC.read(1)
-                                        weightArray.extend([currentWeight])
-                                        print weightArray
-                                        time.sleep(0.3)
-                                        if currentWeight < 10: # or (previousWeight > currentWeight) and ( previousWeight - currentWeight ) > weightError or (previousWeight < currentWeight) and ( currentWeight - previousWeight) > weightError:
-                                                count+=1
-                                                print(u'counter incremented')
-                                                if count >= 10:
-                                                        foodNotFinished = False
-                                        else:
-                                                count = 0
-                                        previousWeight = currentWeight
+				weightArray = []
+				previousWeight = ADC.read(1)
+				count = 0
+				high = 0
+				low = 255
+				foodNotFinished = True
+				while foodNotFinished:
+					currentWeight = ADC.read(1)
+					weightArray.extend([currentWeight])
+					print weightArray
+					high = max(high, currentWeight)
+					if currentWeight > 10:
+						low = min(low, currentWeight)
+        	                        thisOrder = order_ref.document(docs.id)
+	                                thisOrder.update({u'weight': weightArray})
 
-                                print(u'food is finished')
+					time.sleep(0.3)
+					if currentWeight < 10: # or (previousWeight > currentWeight) and ( previousWeight - currentWeight ) > weightError or (previousWeight < currentWeight) and ( currentWeight - previousWeight) > weightError:
+						count+=1
+						print(u'counter incremented')
+						if count >= 5:
+							foodNotFinished = False
+					else:
+						count = 0
+					previousWeight = currentWeight
 
-                                thisOrder = order_ref.document(docs.id)
-                                thisOrder.update({u'weight': weightArray})
+				print(u'food is finished')
 
-                                print(u'------Weight Array uploaded-------')
+				print(u'------Weight Array uploaded-------')
+				print(high - low)
+				db.collection(u'done_orders').document(docs.id).update({u'status': u'completed', u'leftover': high - low})
+#				db.collection(u'done_orders').document(docs.id).set(thisOrder.get().to_dict())
 
-                                # Done order is delievered, not finished eating
-                                db.collection(u'done_orders').document(docs.id).set(thisOrder.get().to_dict())
-#                                db.collection(u'orders').document(docs.id).delete()
+				print(u'Order is completed')
 
-                                print(u'Order is completed and moved to done')
-
-                        except KeyError:
-                                print(u'No such field!')
-        except KeyError:
-                print(u'No such document!')
+			except KeyError:
+				print(u'No such field!')
+	except KeyError:
+		print(u'No such document!')
 
 def loop():
-        while True:
-                print ADC.read(1)
-                scanIfOrderAssociated()
-                time.sleep(0.3)
-                #ADC.write(ADC.read(0))
+	while True:
+		print ADC.read(1)
+		scanIfOrderAssociated()
+		time.sleep(0.3)
 def destroy():
-        ADC.write(0)
+	ADC.write(0)
 
 if __name__ == "__main__":
-        try:
-                setup()
-                loop()
-        except KeyboardInterrupt:
-                destroy()
+	try:
+		setup()
+		loop()
+	except KeyboardInterrupt:
+		destroy()
